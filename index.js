27dcd31411fceafdf8f7eb362a5569fef3483ebf
Jason Humphrey <humphrey.jason32@gmail.com>
2018-03-15 19:24:13 -0500

    added ability append with different options

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 var loop = require('serial-loop')
 var fs = require('fs')
-module.exports = function (files, dest, callback) {
-  fs.writeFile(dest, '', function (error) {
-    if (error) return callback(error)
-    loop(files.length, function (done, i) {
-      fs.readFile(files[i], function (error, buffer) {
-        return error ? done(error) : fs.appendFile(dest, buffer, done)
-      })
-    }, callback)
-  })
+module.exports = function (_options) {
+  var options = Object.assign({}, {
+    first: '',
+    last: '',
+    pre: '',
+    post: ''
+  }, _options)
+  return function (files, dest, callback) {
+    if (!callback) callback = function () {}
+    fs.writeFile(dest, '', function (error) {
+      if (error) return callback(error)
+      loop(files.length, function (done, i) {
+        fs.readFile(files[i], 'utf8', function (error, buffer) {
+          var content
+          if (i === 0) {
+            content = options.first + buffer + options.post
+          } else if (i === (files.length - 1)) {
+            content = options.pre + buffer + options.last
+          } else {
+            content = options.pre + buffer + options.post
+          }
+          return error ? done(error) : fs.appendFile(dest, content, done)
+        })
+      }, callback)
+    })
+  }
 }
+
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert
 var fs = require('fs')
 var path = require('path')
-var concat = require('../')
+
 describe('serial-concat-files', function () {
   before(function (done) {
     if (!fs.existsSync(path.join(__dirname + '/files/'))) {
@@ -11,35 +11,110 @@ describe('serial-concat-files', function () {
     }
   })
   describe('files test', function () {
-    it('3 files A', function (done) {
+    it('3 files POST A', function (done) {
+      var concat = require('../')({ post: '\n'})
       concat(['./test/a', './test/b', './test/c'], './test/files/alla', function (error) {
         assert.isNotOk(error)
 
         fs.readFile('./test/files/alla', function (error, all) {
           assert.isNotOk(error)
-          assert.equal(all.toString(), 'this is a\nthis is b\nand this is c\n')
+          assert.equal(all.toString(), 'this is a\nthis is b\nand this is c')
           done()
         })
       })
     })
-    it('3 files B', function (done) {
+    it('3 files POST B', function (done) {
+      var concat = require('../')({ post: '\n'})
       concat(['./test/b', './test/b', './test/c'], './test/files/allb', function (error) {
         assert.isNotOk(error)
 
         fs.readFile('./test/files/allb', function (error, all) {
           assert.isNotOk(error)
-          assert.equal(all.toString(), 'this is b\nthis is b\nand this is c\n')
+          assert.equal(all.toString(), 'this is b\nthis is b\nand this is c')
           done()
         })
       })
     })
-    it('3 files C', function (done) {
+    it('3 files POST C', function (done) {
+      var concat = require('../')({ post: '\n'})
       concat(['./test/a', './test/b', './test/b'], './test/files/allc', function (error) {
         assert.isNotOk(error)
 
         fs.readFile('./test/files/allc', function (error, all) {
           assert.isNotOk(error)
-          assert.equal(all.toString(), 'this is a\nthis is b\nthis is b\n')
+          assert.equal(all.toString(), 'this is a\nthis is b\nthis is b')
+          done()
+        })
+      })
+    })
+    it('3 files PRE', function (done) {
+      var concat = require('../')({ pre: '\n'})
+      concat(['./test/a', './test/b', './test/b'], './test/files/allc', function (error) {
+        assert.isNotOk(error)
+
+        fs.readFile('./test/files/allc', function (error, all) {
+          assert.isNotOk(error)
+          assert.equal(all.toString(), 'this is a\nthis is b\nthis is b')
+          done()
+        })
+      })
+    })
+    it('3 files PRE FIRST', function (done) {
+      var concat = require('../')({ first: 'GPS MADE THIS\n',pre: '\n'})
+      concat(['./test/a', './test/b', './test/b'], './test/files/allc', function (error) {
+        assert.isNotOk(error)
+
+        fs.readFile('./test/files/allc', function (error, all) {
+          assert.isNotOk(error)
+          assert.equal(all.toString(), 'GPS MADE THIS\nthis is a\nthis is b\nthis is b')
+          done()
+        })
+      })
+    })
+    it('3 files PRE LAST', function (done) {
+      var concat = require('../')({ pre: '\n',last:'\nGPS COPYRIGHT'})
+      concat(['./test/a', './test/b', './test/b'], './test/files/allc', function (error) {
+        assert.isNotOk(error)
+
+        fs.readFile('./test/files/allc', function (error, all) {
+          assert.isNotOk(error)
+          assert.equal(all.toString(), 'this is a\nthis is b\nthis is b\nGPS COPYRIGHT')
+          done()
+        })
+      })
+    })
+    it('3 files POST FIRST', function (done) {
+      var concat = require('../')({ post: '\n',first:'GPS\n'})
+      concat(['./test/a', './test/b', './test/b'], './test/files/allc', function (error) {
+        assert.isNotOk(error)
+
+        fs.readFile('./test/files/allc', function (error, all) {
+          assert.isNotOk(error)
+          assert.equal(all.toString(), 'GPS\nthis is a\nthis is b\nthis is b')
+          done()
+        })
+      })
+    })
+    it('3 files POST LAST', function (done) {
+      var concat = require('../')({ post: '\n',last:'GPS'})
+      concat(['./test/a', './test/b', './test/b'], './test/files/allc', function (error) {
+        assert.isNotOk(error)
+
+        fs.readFile('./test/files/allc', function (error, all) {
+          assert.isNotOk(error)
+          assert.equal(all.toString(), 'this is a\nthis is b\nthis is bGPS')
+          done()
+        })
+      })
+    })
+    it('3 files PRE POST FIRST LAST', function (done) {
+      var concat = require('../')({ post: '<POST>',last:'<LAST>', first:'<FIRST>',pre:'<PRE>'})
+      concat(['./test/a', './test/b', './test/b'], './test/files/allc', function (error) {
+        assert.isNotOk(error)
+
+        fs.readFile('./test/files/allc', function (error, all) {
+          assert.isNotOk(error)
+          assert.equal(all.toString(), '<FIRST>this is a<POST><PRE>this is b<POST><PRE>this is b<LAST>')
           done()
         })
       })
